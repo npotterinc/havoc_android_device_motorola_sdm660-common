38b56582657dd35eedcae6ce19f2af9876b79222
SyberHexen <SyberHexen@gmail.com>
2019-07-10 17:19:26 -0700

    sdm660-common: remove SoundTrigger for now

--- common.mk
+++ common.mk
@@ -36,8 +36,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
     audiod \
     audio.a2dp.default \
     audio.primary.sdm660 \
