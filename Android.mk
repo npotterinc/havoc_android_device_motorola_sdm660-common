60890bdeb4d8d24a089cb921939ac9354343fed7
SyberHexen <SyberHexen@gmail.com>
2019-10-04 19:44:52 -0700

    sdm660-common: symlink FM libs

--- Android.mk
+++ Android.mk
@@ -90,6 +90,16 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
+FM_LIBS := libqcomfm_jni.so
+FM_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/FMRadioService/lib/arm64/,$(notdir $(FM_LIBS)))
+$(FM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "FMRadioService lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FM_SYMLINKS)
+
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM ADSP folder structure: $@"
