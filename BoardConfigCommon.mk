3e2230462c85107489cc9a8f694a8e5192b544b6
SyberHexen <SyberHexen@gmail.com>
2019-10-04 18:33:15 -0700

    sdm660-common: fix dexpreopt
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,14 +143,15 @@ TARGET_USES_COLOR_METADATA := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Enable dexpreopt to speed boot time
+# Dexpreopt
+# Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-    ifneq ($(TARGET_BUILD_VARIANT),eng)
-        ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
-            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-        endif
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+      WITH_DEXPREOPT := true
     endif
+  endif
 endif
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
